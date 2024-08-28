865018d7eea8b796628cf843614d1020fde49747
DIMITRIOS CHRISOS <sdi2100275@di.uoa.gr>
2024-08-28 17:46:03 +0300

    removed depreciation problem from app/api/search

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -3,31 +3,25 @@ import connectMongoDB from "@lib/mongodb";
 import { NextResponse } from 'next/server';
 
 export async function GET(req) {
-    try {
-      const { searchParams } = new URL(req.url);
-      const query = searchParams.get('query');
-  
-      if (!query) {
-        return NextResponse.json({ users: [] }, { status: 200 });
-      }
-  
-      await connectMongoDB();
-  
-      const users = await User.find({
-        $or: [
-          { name: { $regex: query, $options: 'i' } },
-          { surname: { $regex: query, $options: 'i' } },
-        ],
-      });
-  
-      return NextResponse.json({ users }, { status: 200 });
-    } catch (error) {
-      return NextResponse.json({ message: 'Error searching users', error }, { status: 500 });
+  try {
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get('query');
+
+    if (!query) {
+      return NextResponse.json({ users: [] }, { status: 200 });
     }
+
+    await connectMongoDB();
+
+    const users = await User.find({
+      $or: [
+        { name: { $regex: query, $options: 'i' } },
+        { surname: { $regex: query, $options: 'i' } },
+      ],
+    });
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error searching users', error }, { status: 500 });
   }
-  
-  export const config = {
-    api: {
-      bodyParser: false,
-    },
-  };
+}
