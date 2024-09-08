a24b22f2665e2d4b149f305b343719eeb8554f30
DIMITRIOS CHRISOS <sdi2100275@di.uoa.gr>
2024-09-08 19:48:27 +0300

    top home bar updated looks

--- components/TopHomeBar.jsx
+++ components/TopHomeBar.jsx
@@ -5,36 +5,36 @@ import { HiOutlineHome } from "react-icons/hi";
 
 const TopHomeBar = ({ user }) => {
   return (
-    <>
-        <Link href='/home' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineHome size={26}/>
-          <span className="text-base font-semibold">Home</span>
-        </Link>
-        <Link href={`/network/${user._id}`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineUserGroup size={26}/>
-          <span className="text-base font-semibold">Network</span>
-        </Link>
-        <Link href='/listings/' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineBriefcase size={26}/>
-          <span className="text-base font-semibold">Job Listings</span>
-        </Link>
-        <Link href={'/conversations/'} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineChat size={26}/>
-          <span className="text-base font-semibold">Chats</span>
-        </Link>
-        <Link href='/notifications' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineBell size={26}/>
-          <span className="text-base font-semibold">Notifications</span>
-        </Link>
-        <Link href={`/profile_info`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineUserCircle size={26}/>
-          <span className="text-base font-semibold">Profile</span>
-        </Link>
-        <Link href={`/settings/${user._id}`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
-          <HiOutlineCog size={26}/>
-          <span className="text-base font-semibold">Settings</span>
-        </Link>
-    </>
+    <div className="shadow-lg p-7 w-full rounded-lg border-t-4 border-blue-400 flex justify-around">
+      <Link href='/home' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineHome size={24}/>
+        <span className="text-base">Home</span>
+      </Link>
+      <Link href={`/network/${user._id}`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineUserGroup size={24}/>
+        <span className="text-base ">Network</span>
+      </Link>
+      <Link href='/listings/' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineBriefcase size={24}/>
+        <span className="text-base ">Job Listings</span>
+      </Link>
+      <Link href={'/conversations/'} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineChat size={24}/>
+        <span className="text-base">Chats</span>
+      </Link>
+      <Link href='/notifications' className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineBell size={24}/>
+        <span className="text-base">Notifications</span>
+      </Link>
+      <Link href={`/profile_info`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineUserCircle size={24}/>
+        <span className="text-base">Profile</span>
+      </Link>
+      <Link href={`/settings/${user._id}`} className='text-blue-400 transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center'>
+        <HiOutlineCog size={24}/>
+        <span className="text-base">Settings</span>
+      </Link>
+    </div>
   )
 }
 
